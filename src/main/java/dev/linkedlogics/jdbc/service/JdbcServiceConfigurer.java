535713fa72ffc8c8a4e4ef23e984dd5765ae05ff
radjab <radjab@gmail.com>
2023-03-19 08:56:30 +0100

    using consumer and publisher from framework

--- src/main/java/dev/linkedlogics/jdbc/service/JdbcServiceConfigurer.java
+++ src/main/java/dev/linkedlogics/jdbc/service/JdbcServiceConfigurer.java
@@ -1,15 +1,14 @@
 package dev.linkedlogics.jdbc.service;
 
 import dev.linkedlogics.service.ServiceConfigurer;
+import dev.linkedlogics.service.local.QueueSchedulerService;
 
 public class JdbcServiceConfigurer extends ServiceConfigurer {
 	public JdbcServiceConfigurer() {
 		configure(new JdbcContextService());
 		configure(new JdbcQueueService());
 		configure(new JdbcTopicService());
-		configure(new JdbcConsumerService());
-		configure(new JdbcPublisherService());
-		configure(new JdbcSchedulerService());
+		configure(new QueueSchedulerService());
 		configure(new JdbcTriggerService());
 	}
 }
